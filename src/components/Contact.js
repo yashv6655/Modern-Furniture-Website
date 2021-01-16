8f3a32e6120ecc5da049ff3f245cf10a26983ede
Yash Velagapudi <yashv6655@gmail.com>
2021-01-16 14:39:56 -0800

    Added formspree functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join the mailing list</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            voluptatum delectus quisquam quas illum nostrum animi temporibus
+            voluptatibus veniam dolorum.
+          </p>
+          <form
+            action="https://formspree.io/f/xdopbnen"
+            className="contact-form"
+            method="POST"
+          >
+            <input
+              type="email"
+              name="_replyto"
+              className="form-input"
+              placeholder="Email"
+            />
+            <button type="submit" className="submit-btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +95,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
