be65f981efb67fd0a1129c5eb3cffbe9dc6ea401
Yash Velagapudi <yashv6655@gmail.com>
2021-01-20 16:22:16 -0800

    Passing down images as a prop

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -64,7 +64,7 @@ const SingleProductPage = () => {
           Back
         </Link>
         <div className="product-center">
-          <ProductImages />
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
             <Stars />
