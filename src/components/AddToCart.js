f5996f5d73b213ada96ac4b5c5511a67f8fa991d
Yash Velagapudi <yashv6655@gmail.com>
2021-01-20 17:01:47 -0800

    Choose color functionality

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,13 +1,41 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { FaCheck } from 'react-icons/fa'
-import { useCartContext } from '../context/cart_context'
-import AmountButtons from './AmountButtons'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
+import { useCartContext } from "../context/cart_context";
+import AmountButtons from "./AmountButtons";
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
-}
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+  const [mainColor, setMainColor] = useState(colors[0]);
+  const [] = useState();
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span>Colors:</span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <butotn
+                key={index}
+                className={`${
+                  mainColor === color ? "color-btn active" : "color-btn"
+                }`}
+                style={{
+                  background: color,
+                }}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </butotn>
+            );
+          })}
+        </div>
+      </div>
+      <div className="btn-container"></div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   margin-top: 2rem;
@@ -53,5 +81,5 @@ const Wrapper = styled.section`
     margin-top: 1rem;
     width: 140px;
   }
-`
-export default AddToCart
+`;
+export default AddToCart;
