f4216a488b98a0ffddf75f6ce4591d51579f55f3
Yash Velagapudi <yashv6655@gmail.com>
2021-01-20 16:23:04 -0800

    Changing big image when clicked on

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const ProductImages = () => {
-  return <h4>product images</h4>
-}
+const ProductImages = ({ images = [{ url: "" }] }) => {
+  const [main, setMain] = useState(images[0]);
+  return (
+    <Wrapper>
+      <img src={main.url} alt="img" className="main" />
+      <div className="gallery">
+        {images.map((img, index) => {
+          return (
+            <img
+              src={img.url}
+              alt={img.filename}
+              key={index}
+              onClick={() => setMain(images[index])}
+              className={`${img.url === main.url ? "active" : ""}`}
+            />
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .main {
@@ -48,6 +66,6 @@ const Wrapper = styled.section`
       }
     }
   }
-`
+`;
 
-export default ProductImages
+export default ProductImages;
