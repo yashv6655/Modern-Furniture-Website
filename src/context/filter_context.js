657dfc4db540539ea888061bc0fbf076d6617c1a
Yash Velagapudi <yashv6655@gmail.com>
2021-02-14 19:15:20 -0800

    Shipping and clear filters functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -70,10 +70,15 @@ export const FilterProvider = ({ children }) => {
     if (name === "price") {
       value = Number(value);
     }
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
 
-  const clearFilters = (e) => {};
+  const clearFilters = (e) => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
 
   return (
     <FilterContext.Provider
