8ede9a75f9552c564471a6caca551bd2601b8b5e
Yash Velagapudi <yashv6655@gmail.com>
2021-02-28 22:17:45 -0800

    Removed console.log

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -25,8 +25,6 @@ const Filters = () => {
   const companies = getUniqueValues(all_products, "company");
   const colors = getUniqueValues(all_products, "colors");
 
-  console.log(categories, companies, colors);
-
   return (
     <Wrapper>
       <div className="content">
