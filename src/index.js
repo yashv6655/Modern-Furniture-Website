3eca2e36a0e49b60c01c0bc10927e0797305715e
Yash Velagapudi <yashv6655@gmail.com>
2021-03-05 17:27:04 -0800

    Added env variables

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { keys } from "./EnvironmentVariables";
+
+const { DOMAIN, CLIENT_ID } = keys;
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
