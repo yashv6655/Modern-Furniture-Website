c98b9623a51a33a73be3f96c42197c1d007c6f43
Yash Velagapudi <yashv6655@gmail.com>
2021-03-18 21:22:17 -0700

    Setting and passing user through context

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -14,9 +14,11 @@ export const UserProvider = ({ children }) => {
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    console.log(`user: ${user}`);
-    console.log(`isAuth: ${isAuthenticated}`);
-    console.log(`loading: ${isLoading}`);
+    if (isAuthenticated) {
+      setMyUser(user);
+    } else {
+      setMyUser(false);
+    }
   }, [isAuthenticated]);
 
   return (
