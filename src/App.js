8a4e25d587279d9056cfae47c66365f4ab1fd910
Yash Velagapudi <yashv6655@gmail.com>
2021-03-27 20:57:36 -0700

    Fixed checkout page auth bug

--- src/App.js
+++ src/App.js
@@ -10,24 +10,27 @@ import {
   PrivateRoute,
   ProductsPage,
   SingleProductPage,
+  AuthWrapper,
 } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/" children={<HomePage />} />
-        <Route exact path="/about" children={<AboutPage />} />
-        <Route exact path="/cart" children={<CartPage />} />
-        <Route exact path="/products" children={<ProductsPage />} />
-        <Route exact path="/products/:id" children={<SingleProductPage />} />
-        <PrivateRoute exact path="/checkout" children={<CheckoutPage />} />
-        <Route exact path="*" children={<ErrorPage />} />
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/" children={<HomePage />} />
+          <Route exact path="/about" children={<AboutPage />} />
+          <Route exact path="/cart" children={<CartPage />} />
+          <Route exact path="/products" children={<ProductsPage />} />
+          <Route exact path="/products/:id" children={<SingleProductPage />} />
+          <PrivateRoute exact path="/checkout" children={<CheckoutPage />} />
+          <Route exact path="*" children={<ErrorPage />} />
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
