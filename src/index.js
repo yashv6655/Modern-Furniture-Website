e01fcbc2b1fda27314a85e140880cd39f31f0741
Yash Velagapudi <yashv6655@gmail.com>
2021-04-06 11:19:19 -0700

    fixed env bug

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { keys } from "./EnvironmentVariables";
-
-const { DOMAIN, CLIENT_ID } = keys;
 
 ReactDOM.render(
   <Auth0Provider
-    domain={DOMAIN}
-    clientId={CLIENT_ID}
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
