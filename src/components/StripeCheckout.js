0491fc5e51f87dbb98fb1e3f73dd144bf4f73cab
Yash Velagapudi <yashv6655@gmail.com>
2021-04-13 22:07:32 -0700

    Set up client secret for checkout

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -49,12 +49,17 @@ const CheckoutForm = () => {
 
   const createPaymentIntent = async () => {
     try {
-      const data = await axios.post(
+      const { data } = await axios.post(
         "/.netlify/functions/create-payment-intent",
         JSON.stringify({ cart, shipping_fee, total_amount })
       );
-    } catch (error) {}
+      setClientSecret(data.clientSecret);
+      // console.log(data.clientSecret);
+    } catch (error) {
+      // console.log(error.response);
+    }
   };
+
   useEffect(() => {
     createPaymentIntent();
     // eslint-disable-next-line
